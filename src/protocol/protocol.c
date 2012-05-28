3975c174b47bdf01e6d1a5bafe8302214aaa6004
renyongquan <yongquan@staff.sina.com.cn>
2012-05-28 11:34:12 +0800

    Hard code saltleft=13 in readHandShakePkt()

--- src/protocol/protocol.c
+++ src/protocol/protocol.c
@@ -49,7 +49,8 @@ int readHandshakePkt(HandshakePkt *pkt,size_t len,int fd){
 	readBinary(&io,salt2,10);
 	if(saltlen>pkt->saltlen){
 		/*plus 1 for '\0'*/
-		saltleft = saltlen - pkt->saltlen + 1; 
+	//	saltleft = saltlen - pkt->saltlen + 1;  /*Ignore this,mysql5.5 saltlen=21 ,but this looks like useless*/
+		saltleft = 13;
 	}
 	else{
 		saltleft = 13;
